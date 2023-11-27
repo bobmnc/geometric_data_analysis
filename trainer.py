6cbddcb5c8cbf93fb6b65a6015407cf810882962
bobmnc <bobomnc2b@gmail.com>
2023-11-27 18:53:00 +0100

    better train function

--- trainer.py
+++ trainer.py
@@ -2,27 +2,26 @@ import torch.nn as nn
 from torch import optim
 from models import Original_model
 from dataset import UPFD
+from tqdm import tqdm
 from torch_geometric.loader import DataLoader
+import torch
 
 def train(model,train_dataloader,test_dataloader,N_epochs,device='cuda:0'):
     loss_fn = nn.CrossEntropyLoss()
     
     model = model.to(device)
-    optimizer = optim.Adam(model.parameters(),lr=0.01)
-    for epoch in range(N_epochs):
+    optimizer = optim.Adam(model.parameters(),lr=0.0001)
+    for epoch in tqdm(range(N_epochs)):
+        model.train()
         tot_loss = 0
         n_corrects = 0
         n_tot = 0
         for batch in train_dataloader:
-            print(batch)
             y = batch.y
-
             optimizer.zero_grad()
             batch = batch.to(device)
             y = y.to(device)
             out = model(batch)
-            print(out.shape)
-            print(y.shape)
             loss = loss_fn(out,y)
             loss.backward()
             optimizer.step()
@@ -30,18 +29,41 @@ def train(model,train_dataloader,test_dataloader,N_epochs,device='cuda:0'):
             pred = out.argmax(dim=-1)
             n_tot+= len(pred)
             n_corrects+= (pred==y).sum()
+        if epoch%5==0:
+            print('Epoch {} loss : {:.3f} accuracy :{} '.format(epoch,
+                                                                loss/n_tot,
+                                                                n_corrects/n_tot))
+        with torch.no_grad():
+            for batch in test_dataloader:
+                
+                y = batch.y
+                batch = batch.to(device)
+                y = y.to(device)
+                out = model(batch)
+                loss = loss_fn(out,y)
 
-        print('Epoch {} loss : {:.3f} accuracy :{} '.format(epoch,
-                                                            loss/n_tot,
-                                                            n_corrects/n_tot))
+                tot_loss +=loss.cpu().detach().item()
+                pred = out.argmax(dim=-1)
+                n_tot+= len(pred)
+                n_corrects+= (pred==y).sum()
+            if epoch%5==0:
+                print('Test set loss : {:.3f} accuracy :{} '.format(loss/n_tot,
+                                                                    n_corrects/n_tot))
 if __name__=='__main__':
-    dataset = UPFD('.','politifact','profile')
-    model = Original_model(dataset)
+    train_dataset = UPFD('.',
+                         'politifact',
+                         'profile')
+    test_dataset = UPFD('.',
+                        'politifact',
+                        'profile',
+                        'test')
+    model = Original_model(train_dataset)
     device = 'cpu'
-    train_dataloader = DataLoader(dataset,batch_size=3)
+    train_dataloader = DataLoader(train_dataset,batch_size=4)
+    test_dataloader = DataLoader(test_dataset,batch_size=4)
     train(model=model,
           train_dataloader=train_dataloader,
           test_dataloader=train_dataloader,
           device=device,
-          N_epochs=1)
+          N_epochs=200)
 
