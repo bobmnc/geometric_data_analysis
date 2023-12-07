e0649161cfabe6079017a5fbd018fe97476b4aee
bobmnc <bobomnc2b@gmail.com>
2023-12-07 15:32:43 +0100

    fix bugs

--- create_embed_dataset.py
+++ create_embed_dataset.py
@@ -5,34 +5,42 @@ from torch_geometric.loader import DataLoader
 from tqdm import tqdm
 import torch
 import h5py
+import argparse
 
-dict_models = {'original':models.Original_model()}
+dict_models = {'original':models.Original_model}
 
 def create_embedding_dataset(model_checkpoint,model_class):
     model = dict_models[model_class]
     checkpoint = torch.load(model_checkpoint)
+
+    description = checkpoint['description_experiment']
+    print(description)
+
+    train_dataset = UPFD('.',
+                         description['dataset_name'],
+                         description['features'],
+                         'train')
+    test_dataset = UPFD('.',
+                        description['dataset_name'],
+                        description['features'],
+                        'test')
+    model = model(dataset=train_dataset)
+
     model.load_state_dict(checkpoint['model_state_dict'])
     epoch = checkpoint['epoch']
     print('Model retrieved at epoch {}'.format(epoch))
-    test_loss = model_checkpoint['loss']
-    test_accuracy = model_checkpoint['accuracy']
+    test_loss = checkpoint['loss']
+    test_accuracy = checkpoint['accuracy']
     print('loss {:.3f}, accuracy {:.3f}'.format(test_loss,test_accuracy))
-    description = model_checkpoint['description_experiment']
+    
     device = 'cpu'
-    train_dataset = UPFD('.',
-                         description.dataset_name,
-                         description.features,
-                         'train')
-    test_dataset = UPFD('.',
-                        description.dataset,
-                        description.features,
-                        'test')
+    
     
     
     train_dataloader = DataLoader(train_dataset,
-                                  batch_size=description.batch_size)
+                                  batch_size=description['batch_size'])
     test_dataloader = DataLoader(test_dataset,
-                                 batch_size=description.batch_size)
+                                 batch_size=description['batch_size'])
     model.eval()
     with torch.no_grad():
         list_out_train = list()
@@ -44,7 +52,7 @@ def create_embedding_dataset(model_checkpoint,model_class):
                             return_embed=True)
                 out = out.numpy()
                 list_out_train.append(out)
-        list_out_train = np.array(list_out_train)
+        list_out_train = np.concatenate(list_out_train)
         list_out_train = np.reshape(list_out_train,(1,-1))
         with h5py.File("embeddings_graphs.hdf5", "w") as f:
             f.create_dataset("train_embeds",data = list_out_train)
@@ -59,10 +67,20 @@ def create_embedding_dataset(model_checkpoint,model_class):
                             return_embed=True)
                 out = out.numpy()
                 list_out_test.append(out)
-        list_out_test = np.array(list_out_test)
+        list_out_test = np.concatenate(list_out_test)
         list_out_test = np.reshape(list_out_test,(1,-1))
         with h5py.File("embeddings_graphs.hdf5", "w") as f:
             f.create_dataset("test_embeds",data = list_out_test)
 
                 
+if __name__=='__main__':
+    parser = argparse.ArgumentParser(description="Params for training models")
 
+    # Common params
+    parser.add_argument("--model_checkpoint", help="Directory to save the models",
+                        type =str,
+                        default='models/best_model__politifact_profile_8.pt')
+    
+    args  = parser.parse_args()
+    create_embedding_dataset(model_checkpoint=args.model_checkpoint,
+                             model_class='original')
