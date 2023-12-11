92ec26ad2806b04f1ea2fc15df641a9b79a7434a
bobmnc <bobomnc2b@gmail.com>
2023-12-11 15:48:09 +0100

    added model with graph attention

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 
 import torch
 import torch.nn.functional as F
-from torch_geometric.nn import GCNConv
+from torch_geometric.nn import GCNConv,GATConv
 from torch_geometric.nn.pool import global_mean_pool
 from torch.nn import Linear,Softmax
 
@@ -68,4 +68,34 @@ class Deepset(torch.nn.Module):
         x = self.linear2(x)
         return self.softmax(x)
 
+class Model_attention(torch.nn.Module):
+    def __init__(self,dataset):
+        super().__init__()
+        self.conv1 = GATConv(dataset.num_node_features, 64,
+                             heads=1)
+        self.conv2 = GATConv(64, 64,
+                             heads=1)
+        self.linear1 = Linear(64,32)
+        self.linear2 = Linear(32,2)
+        self.softmax = Softmax(dim=0)
+        self.pooling = global_mean_pool
+        
+    def forward(self, data,return_embed = False):
+        x, edge_index = data.x, data.edge_index
+        batch_vev = data.batch
+
+        x = self.conv1(x,
+                    edge_index)
+        x = F.selu(x)
+        x = F.dropout(x,
+                       training=self.training)
+        x = self.conv2(x, 
+                       edge_index)
+        x = self.pooling(x,
+                         batch=batch_vev)
+        if return_embed :
+            return x
+        x = self.linear1(x)
+        x = self.linear2(x)
+        return self.softmax(x)
 
